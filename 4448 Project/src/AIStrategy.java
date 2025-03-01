1a32fbca84ca365e609733db244b4588d6d3a93e
JohnLe11 <165092723+JohnLe11@users.noreply.github.com>
2025-02-28 23:24:06 -0700

    Delete 4448 Project directory

--- 4448 Project/src/AIStrategy.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-public interface AIStrategy {
-
-    public String pickCard(ArrayList<String> selectedCard, ArrayList<Card> computerHand);
-
-    
-}
--- 4448 Project/src/HandBuilder.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-public interface HandBuilder {
-    public void addCard(Card card);
-    public ArrayList<Card> getCards();
-}
